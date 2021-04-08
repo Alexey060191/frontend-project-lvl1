38b16a8ea4db987137d1c56a1531cf80de9f59b3
Alexey Yatsin <yatsin91@mail.ru>
2021-04-08 22:37:54 +0600

    add brain-games.js

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,2 +1,7 @@
 #!/usr/bin/env node
+// import { name } from '../src/cli.js';
+import { hellouser } from '../src/cli.js';
+
 console.log("Welcome to the Brain Games!");
+// console.log(`Hello, ${name}!`);
+hellouser();
