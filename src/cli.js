fc2f0f8bc2542148f5ff5a775d45d348f1568352
Alexey Yatsin <yatsin91@mail.ru>
2021-04-10 16:11:29 +0600

    fix brain-games&cli

--- src/cli.js
+++ src/cli.js
@@ -2,7 +2,7 @@ import readlineSync from 'readline-sync';
 
 // export const name = readlineSync.question('May I have your name? ');
 
-export const hellouser = () => {
-    const name = readlineSync.question('May I have your name? ');
-    console.log(`Hello, ${name}!`);
+export default () => {
+  const name = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${name}!`);
 };
